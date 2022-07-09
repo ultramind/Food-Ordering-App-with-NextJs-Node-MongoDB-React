504521b3de2659627aa9020fdc87f40e4254759d
ultragrace <udechukwuakachukwu.com>
2022-07-10 00:33:06 +0100

    Created the Footer Component

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,54 @@
-
+import styles from '../styles/Footer.module.css'
+import Image from 'next/image'
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className={styles.container}>
+      <div className={styles.items}>
+        <Image src='/img/footer-bg.jpg' layout='fill' alt='' />
+      </div>
+      <div className={styles.items}>
+        <div className={styles.card}>
+          <h2 className={styles.motor}>
+            OH YES WE DID IT, LAMA PIZZA, WELL KNOW PIZZA IN THE WORLD AND THE
+            MOST DEICIOUS ONE
+          </h2>
+        </div>
+        <div className={styles.card}>
+          <h2 className={styles.title}>FIND OUR RESTURANTS</h2>
+          <p className={styles.text}>
+            1654 R. Don Road #458.
+            <br />
+            NewYork, 85022
+            <br /> (602) 867-1010
+          </p>
+          <p className={styles.text}>
+            1654 R. Don Road #458.
+            <br />
+            NewYork, 85022
+            <br /> (602) 867-1010
+          </p>
+          <p className={styles.text}>
+            1654 R. Don Road #458.
+            <br />
+            NewYork, 85022
+            <br /> (602) 867-1010
+          </p>
+        </div>
+        <div className={styles.card}>
+        <h2 className={styles.title}>WORKING HOURS</h2>
+          <p className={styles.text}>
+            MONDAY UNTIL FRIDAY
+            <br />
+            9:00 - 22:00
+          </p>
+          <p className={styles.text}>
+            SATURDAY UNTIL SUNDAY
+            <br />
+            12:00 - 24:00
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
